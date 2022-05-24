204f2b00813de30eab8ab6e979b8bb6a4cd395d1
ant-lazo <ant.14sag@gmail.com>
2022-05-24 16:10:52 -0500

    new changes

---
+++ src/main/java/com/nttdata/bootcamp/models/products/CurrentAccount.java
@@ -0,0 +1,23 @@
+package com.nttdata.bootcamp.models.products;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "currentaccounts")
+@Builder
+public class CurrentAccount {
+	@Id
+	private String id;
+	private String idCustomer;
+	private String typeCustomer;
+	private String accountingBalance;
+	private String maintenance;
+}
